ef6b9342474b7acee65f9a401c2d29a4d7e1cb1a
contatokaioassis-sudo <contatokaioassis@gmail.com>
2025-11-25 22:41:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+app.use(cors({ origin: "*", methods: ["POST", "GET"], allowedHeaders: ["Content-Type", "Authorization"] }));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // VARIÁVEIS CORRETAS
 const PAYEVO_SECRET = process.env.PAYEVO_SECRET_KEY;
